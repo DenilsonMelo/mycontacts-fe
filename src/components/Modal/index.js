34438ca9add48294407cd6831b499310418900bf
DenilsonMelo <denilsonmlo42@gmail.com>
2023-07-03 09:26:36 -0300

    refactor: create reactportal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,10 +1,9 @@
-import ReactDOM from 'react-dom';
-
 import PropTypes from 'prop-types';
 
-import { Container, Overlay, Footer } from './styles';
-
 import Button from '../Button';
+import ReactPortal from '../ReactPortal';
+
+import { Container, Overlay, Footer } from './styles';
 
 export default function Modal({
   danger, title, children, cancelLabel, confirmLabel, onCancel, onConfirm, visible, isLoading,
@@ -13,34 +12,35 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container $danger={danger}>
-        <h1>{title}</h1>
-        <div className="modal-body">
-          {children}
-        </div>
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
-          <Button
-            type="button"
-            danger={danger}
-            onClick={onConfirm}
-            isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container $danger={danger}>
+          <h1>{title}</h1>
+          <div className="modal-body">
+            {children}
+          </div>
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
